84d4c8205113024d66c89f9268ec413dc19cd640
Geeth <induwarakwg3573@gmail.com>
2023-11-13 21:31:22 +0530

    Install tailwindcss and remove unused files

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      <div>
-      <h1>NChapters</h1>
-      </div>
-      
-      
-        
-        
-    </>
+    <h1 className='text-red-700'>
+      Hello
+    </h1>
   )
 }
-
-export default App

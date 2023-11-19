c6c4e2cd1d9d10626b0d0645fa10dae93706899a
Geeth <induwarakwg3573@gmail.com>
2023-11-19 15:42:44 +0530

    Complete sign up page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,15 +1,39 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function SignUp() {
+  //keep track the all data
+  const [formData, setFormData] = useState({});
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    //prevent refreshing the page when we submit
+    e.preventDefault();
+    const res = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    });
+    const data = await res.json();
+    console.log(data);
+  };
+  console.log(formData);
   return (
     <div className="max-w-lg p-3 mx-auto">
       <h1 className="text-3xl font-semibold text-center my-7">SignUp</h1>
-      <form action="" className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} action="" className="flex flex-col gap-4">
         <input
           type="text"
           placeholder="Index Number"
           className="p-3 border rounded-lg"
           id="indexno"
+          onChange={handleChange}
         />
 
         <input
@@ -17,6 +41,7 @@ export default function SignUp() {
           placeholder="Username"
           className="p-3 border rounded-lg"
           id="username"
+          onChange={handleChange}
         />
 
         <input
@@ -24,6 +49,7 @@ export default function SignUp() {
           placeholder="Email"
           className="p-3 border rounded-lg"
           id="email"
+          onChange={handleChange}
         />
 
         <input
@@ -31,6 +57,7 @@ export default function SignUp() {
           placeholder="Passsword"
           className="p-3 border rounded-lg"
           id="password"
+          onChange={handleChange}
         />
         <button className="p-3 text-white uppercase rounded-lg cursor-pointer bg-slate-700 hover:opacity-95 disabled:opacity-80">
           Sign up

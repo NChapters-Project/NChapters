e4ad5bfb11e113af5ff61c6141de7e60afe9e7ae
Rashminda <rashminda121@gmail.com>
2024-02-08 20:48:34 +0530

    added about us text

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -3,8 +3,14 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      <h1>About page</h1>
+    <div class="bod">
+      <div className="nsbm mt-10 flex justify-center items-center">
+        <div className="">
+          <h1 className="text-5xl txt-col">About Us</h1>
+        </div>
+      </div>
+
+
     </div>
   )
 }

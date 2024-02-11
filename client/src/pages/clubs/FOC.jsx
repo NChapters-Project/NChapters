ad10be9b87b329cf383200c7e58bd2c965a1ada4
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-02-11 17:08:35 +0530

    developed hero section of FOC page

---
+++ client/src/pages/clubs/FOC.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function FOC() {
+  return (
+    <div>
+        
+        
+
+<section class="bg-center bg-no-repeat bg-[url(src/images/com.jpg)] bg-gray-700 bg-blend-multiply mt-12">
+    <div class="px-4 mx-auto max-w-screen-xl h-[20rem] text-center py-12 lg:py-20">
+        <h3 class="mt-8 text-2xl font-extrabold tracking-tight leading-none text-white md:text-2xl lg:text-5xl">Faculty of Computing</h3>
+        <p class="mt-6 text-lg font-normal text-gray-300 lg:text-3xl sm:px-16 lg:px-48">Clubs & Societies</p>
+        
+    </div>
+</section>
+
+{/* <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mt-20">
+    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt=""/>
+    <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">FOSS Community</h5>
+        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+    </div>
+</a> */}
+
+    </div>
+  )
+}
+
+export default FOC

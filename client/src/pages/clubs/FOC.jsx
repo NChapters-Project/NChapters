eb5e1e12b224933273f8a645eaef7e0aa3938a0e
Jayawardana OV <ovjayawardana@students.nsbm.ac.lk>
2024-02-11 20:17:52 +0530

    completed FOC page

--- client/src/pages/clubs/FOC.jsx
+++ client/src/pages/clubs/FOC.jsx
@@ -7,20 +7,53 @@ function FOC() {
         
 
 <section class="bg-center bg-no-repeat bg-[url(src/images/com.jpg)] bg-gray-700 bg-blend-multiply mt-12">
-    <div class="px-4 mx-auto max-w-screen-xl h-[20rem] text-center py-12 lg:py-20">
+    <div class="px-4 mx-auto max-w-screen-xl md:h-[20rem] sm:h-[15rem] text-center py-12 lg:py-20">
         <h3 class="mt-8 text-2xl font-extrabold tracking-tight leading-none text-white md:text-2xl lg:text-5xl">Faculty of Computing</h3>
         <p class="mt-6 text-lg font-normal text-gray-300 lg:text-3xl sm:px-16 lg:px-48">Clubs & Societies</p>
         
     </div>
 </section>
 
-{/* <a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mt-20">
-    <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src="/docs/images/blog/image-4.jpg" alt=""/>
+<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-6xl min-h-[20rem] hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mt-20 ml-3 mr-3">
+    <img class="object-cover w-full rounded-t-lg h-96 md:h-[20rem] md:w-[35rem] md:rounded-none md:rounded-s-lg" src="src/images/clubs/foss.jpeg" alt=""/>
     <div class="flex flex-col justify-between p-4 leading-normal">
-        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">FOSS Community</h5>
-        <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+        <h5 class="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">FOSS Community</h5>
+        <p class="mb-3 text-xl font-normal text-gray-700 dark:text-gray-400">Contribute to the society with developments and new concepts transforming young lives to feel as a part of the world's largest developing community.</p>
     </div>
-</a> */}
+</a>
+
+<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-6xl min-h-[20rem] hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mt-20 md:ml-auto ml-3 mr-3">
+    
+    <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5 class="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">IEEE Student Branch</h5>
+        <p class="mb-3 text-xl font-normal text-gray-700 dark:text-gray-400">Provides opportunities to meet and learn from fellow IEEE Student and Graduate Student Members and engage with professional IEEE members locally.</p>
+    </div>
+    <img class="object-cover w-full h-96 md:h-[20rem] md:w-[35rem]" src="src/images/clubs/iee.jpg" alt=""/>
+</a>
+
+<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-6xl min-h-[20rem] hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mt-20 ml-3 mr-3">
+    <img class="object-cover w-full rounded-t-lg h-96 md:h-[20rem] md:w-[35rem] md:rounded-none md:rounded-s-lg" src="src/images/clubs/foss.jpeg" alt=""/>
+    <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5 class="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">CSSL Genz Chapter</h5>
+        <p class="mb-3 text-xl font-normal text-gray-700 dark:text-gray-400">Contribute to the society with developments and new concepts transforming young lives to feel as a part of the world's largest developing community.</p>
+    </div>
+</a>
+<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-6xl min-h-[20rem] hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mt-20 md:ml-auto ml-3 mr-3">
+    
+    <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5 class="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">Department of Software Engineering Circle</h5>
+        <p class="mb-3 text-xl font-normal text-gray-700 dark:text-gray-400">Provides opportunities to meet and learn from fellow IEEE Student and Graduate Student Members and engage with professional IEEE members locally.</p>
+    </div>
+    <img class="object-cover w-full h-96 md:h-[20rem] md:w-[35rem]" src="src/images/clubs/iee.jpg" alt=""/>
+</a>
+
+<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-6xl min-h-[20rem] hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mt-20 ml-3 mr-3">
+    <img class="object-cover w-full rounded-t-lg h-96 md:h-[20rem] md:w-[35rem] md:rounded-none md:rounded-s-lg" src="src/images/clubs/foss.jpeg" alt=""/>
+    <div class="flex flex-col justify-between p-4 leading-normal">
+        <h5 class="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">ISAACA</h5>
+        <p class="mb-3 text-xl font-normal text-gray-700 dark:text-gray-400">Contribute to the society with developments and new concepts transforming young lives to feel as a part of the world's largest developing community.</p>
+    </div>
+</a>
 
     </div>
   )

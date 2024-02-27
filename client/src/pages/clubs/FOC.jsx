e0e3f92c36aa7dd0eec7b2452affa1646a96f69d
avish <avishmadushanka21@gmail.com>
2024-02-27 14:25:47 +0530

    update foc page

--- client/src/pages/clubs/FOC.jsx
+++ client/src/pages/clubs/FOC.jsx
@@ -20,8 +20,8 @@ function FOC() {
         <h5 class="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">FOSS Community</h5>
         <p class="mb-3 text-xl font-normal text-gray-700 dark:text-gray-400">Contribute to the society with developments and new concepts transforming young lives to feel as a part of the world's largest developing community.</p>
         <div class="flex">
-            <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{ maxWidth: '220px', marginTop: '1rem' }}>Explore More</button>
-            <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{ maxWidth: '220px', marginTop: '1rem' }}>Turn on notifications</button>
+            <button a href="#" type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{ maxWidth: '220px', marginTop: '1rem' }}>Explore More</button>
+            <button a href="/FOSS" type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{ maxWidth: '220px', marginTop: '1rem' }}>Turn on notifications</button>
         </div>
     </div>
 </a>
@@ -32,8 +32,8 @@ function FOC() {
         <h5 class="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">IEEE Student Branch</h5>
         <p class="mb-3 text-xl font-normal text-gray-700 dark:text-gray-400">Provides opportunities to meet and learn from fellow IEEE Student and Graduate Student Members and engage with professional IEEE members locally.</p>
         <div class="flex">
-            <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{ maxWidth: '220px', marginTop: '1rem' }}>Explore More</button>
-            <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{ maxWidth: '220px', marginTop: '1rem' }}>Turn on notifications</button>
+            <button a href="#" type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{ maxWidth: '220px', marginTop: '1rem' }}>Explore More</button>
+            <button a href="/IEEE" type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{ maxWidth: '220px', marginTop: '1rem' }}>Turn on notifications</button>
         </div>
     </div>
     <img class="object-cover w-full h-96 md:h-[20rem] md:w-[35rem]" src="src/images/clubs/iee.jpg" alt=""/>
@@ -45,8 +45,8 @@ function FOC() {
         <h5 class="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">CSSL Genz Chapter</h5>
         <p class="mb-3 text-xl font-normal text-gray-700 dark:text-gray-400">Contribute to the society with developments and new concepts transforming young lives to feel as a part of the world's largest developing community.</p>
         <div class="flex">
-            <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{ maxWidth: '220px', marginTop: '1rem' }}>Explore More</button>
-            <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{ maxWidth: '220px', marginTop: '1rem' }}>Turn on notifications</button>
+            <button a href="#" type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{ maxWidth: '220px', marginTop: '1rem' }}>Explore More</button>
+            <button a href="/CSSL" type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{ maxWidth: '220px', marginTop: '1rem' }}>Turn on notifications</button>
         </div>
     </div>
 </a>
@@ -56,8 +56,8 @@ function FOC() {
         <h5 class="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">ISACA</h5>
         <p class="mb-3 text-xl font-normal text-gray-700 dark:text-gray-400">Provides opportunities to meet and learn from fellow IEEE Student and Graduate Student Members and engage with professional IEEE members locally.</p>
         <div class="flex">
-            <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{ maxWidth: '220px', marginTop: '1rem' }}>Explore More</button>
-            <button type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{ maxWidth: '220px', marginTop: '1rem' }}>Turn on notifications</button>
+            <button a href="#" type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{ maxWidth: '220px', marginTop: '1rem' }}>Explore More</button>
+            <button a href="/ISACA" type="button" class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2" style={{ maxWidth: '220px', marginTop: '1rem' }}>Turn on notifications</button>
         </div>
     </div>
     <img class="object-cover w-full h-96 md:h-[20rem] md:w-[35rem]" src="src/images/clubs/isaca.jpeg" alt=""/>

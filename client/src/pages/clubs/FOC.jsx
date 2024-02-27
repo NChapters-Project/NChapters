c59bd9b2ddde0fe7299acac34346ca29442c585f
avish <avishmadushanka21@gmail.com>
2024-02-27 12:21:19 +0530

    create foss page

--- client/src/pages/clubs/FOC.jsx
+++ client/src/pages/clubs/FOC.jsx
@@ -14,7 +14,7 @@ function FOC() {
     </div>
 </section>
 
-<a href="#" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-6xl min-h-[20rem] hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mt-20 ml-3 mr-3">
+<a href="/Foss" class="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-6xl min-h-[20rem] hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mt-20 ml-3 mr-3">
     <img class="object-cover w-full rounded-t-lg h-96 md:h-[20rem] md:w-[35rem] md:rounded-none md:rounded-s-lg" src="src/images/clubs/nmc.jpg" alt=""/>
     <div class="flex flex-col justify-between p-4 leading-normal">
         <h5 class="mb-2 text-4xl font-bold tracking-tight text-gray-900 dark:text-white">FOSS Community</h5>

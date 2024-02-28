38d26c612bbe85e5dd5b99cd37a9f122b8d8a5c8
avish madushanka <avishmadushanka21@gmail.com>
2024-02-28 21:05:50 +0530

    update

--- client/src/pages/clubs/Clubadd.jsx
+++ client/src/pages/clubs/Clubadd.jsx
@@ -44,7 +44,7 @@ function Clubadd() {
                   <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
                   <textarea id="description" rows="8" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Your description here"></textarea>
               </div>
-              <div class="w-full">
+              <div class="sm:col-span-2">
                   <label for="brand" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Brand</label>
                   <input type="file" name="brand" id="brand" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Product brand" required=""/>
               </div>

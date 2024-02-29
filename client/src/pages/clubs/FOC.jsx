ff496c90fa7695eb06b42f48c9f7d6abea16e3dd
avish <avishmadushanka21@gmail.com>
2024-02-29 09:57:11 +0530

    update slider

--- client/src/pages/clubs/FOC.jsx
+++ client/src/pages/clubs/FOC.jsx
@@ -27,7 +27,13 @@ function FOC() {
   return (
 
     <div>
+        <section class="bg-center bg-no-repeat bg-[url(src/images/com.jpg)] bg-gray-700 bg-blend-multiply mt-12">
+    <div class="px-4 mx-auto max-w-screen-xl md:h-[20rem] sm:h-[15rem] text-center py-12 lg:py-20">
+        <h3 class="mt-8 text-2xl font-extrabold tracking-tight leading-none text-white md:text-2xl lg:text-5xl">Faculty of Computing</h3>
+        <p class="mt-6 text-lg font-normal text-gray-300 lg:text-3xl sm:px-16 lg:px-48">Clubs & Societies</p>
         
+    </div>
+</section>
       {clubs.map((club, index) => (
         <a key={club.id} href={`/clubs/${club.id}`} className={`flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-6xl min-h-[20rem] hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 mt-20 ml-3 mr-3 ${index % 2 === 0 ? '' : 'md:ml-auto'}`}>
           {index % 2 === 0 ? (

f8197e8fa1eea9d0c32cd68ddd9e06abc7f862eb
Rashminda <rashminda121@gmail.com>
2024-03-03 16:10:04 +0530

    update event edit page

--- client/src/pages/EventEdit.jsx
+++ client/src/pages/EventEdit.jsx
@@ -11,6 +11,7 @@ function EventEdit() {
     eventName: '',
     time: '',
     date: '',
+    minidescription: '',
     description: '',
     image: null,
     clubName: ''
@@ -36,23 +37,23 @@ function EventEdit() {
   };
 
   const confirmDelete = () => {
-    // Perform deletion logic here
     const database = getDatabase();
     const eventRef = ref(database, `events/${deleteEventId}`);
     remove(eventRef)
       .then(() => {
-        // Remove the deleted event from the state
         setEvents(events.filter(event => event.id !== deleteEventId));
-        setDeleteEventId(null); // Reset deleteEventId state
+        setDeleteEventId(null);
+        window.alert('Event deleted successfully!');
       })
       .catch((error) => {
         console.error('Error deleting event: ', error);
-        setDeleteEventId(null); // Reset deleteEventId state
+        setDeleteEventId(null);
+        window.alert('Failed to delete event. Please try again later.');
       });
   };
 
   const cancelDelete = () => {
-    setDeleteEventId(null); // Reset deleteEventId state
+    setDeleteEventId(null);
   };
 
   const handleEdit = (event) => {
@@ -61,11 +62,12 @@ function EventEdit() {
       eventName: event.eventName,
       time: event.time,
       date: event.date,
+      minidescription: event.minidescription,
       description: event.description,
       clubName: event.clubName,
-      image: null, // Reset image to null to avoid displaying the existing image when editing
+      image: null,
     });
-    setIsModalOpen(true); // Open the modal for editing
+    setIsModalOpen(true);
   };
 
   const handleChange = (e) => {
@@ -81,57 +83,56 @@ function EventEdit() {
     const database = getDatabase();
     const eventRef = ref(database, `events/${editEvent.id}`);
   
-    // Create a new object to hold the updated event data
     const updatedEventData = {
       eventName: formData.eventName,
       time: formData.time,
       date: formData.date,
+      minidescription: formData.minidescription,
       description: formData.description,
       clubName: formData.clubName,
     };
   
-    // Check if an image was uploaded
     if (formData.image) {
       const storage = getStorage();
       const imagesRef = storageRef(storage, `images/${formData.image.name}`);
   
-      // Upload the image to Firebase Storage
       uploadBytes(imagesRef, formData.image)
         .then((snapshot) => {
           return getDownloadURL(snapshot.ref);
         })
         .then((imageUrl) => {
-          // Once the image is uploaded, update the event data with the image URL
           updatedEventData.imageUrl = imageUrl;
   
-          // Update the event data in the Firebase Realtime Database
           update(eventRef, updatedEventData)
             .then(() => {
               console.log('Event updated successfully!');
-              setIsModalOpen(false); // Close the modal
-              setEditEvent(null); // Reset editEvent state
+              setIsModalOpen(false);
+              setEditEvent(null);
+              window.alert('Event updated successfully!');
             })
             .catch((error) => {
               console.error('Error updating event: ', error);
+              window.alert('Failed to update event. Please try again later.');
             });
         })
         .catch((error) => {
           console.error('Error uploading image: ', error);
+          window.alert('Failed to upload image. Please try again later.');
         });
     } else {
-      // If no image was uploaded, update the event data directly
       update(eventRef, updatedEventData)
         .then(() => {
           console.log('Event updated successfully!');
-          setIsModalOpen(false); // Close the modal
-          setEditEvent(null); // Reset editEvent state
+          setIsModalOpen(false);
+          setEditEvent(null);
+          window.alert('Event updated successfully!');
         })
         .catch((error) => {
           console.error('Error updating event: ', error);
+          window.alert('Failed to update event. Please try again later.');
         });
     }
   };
-  
 
   const closeModal = () => {
     setIsModalOpen(false);
@@ -140,7 +141,6 @@ function EventEdit() {
   return (
     <div className="relative overflow-x-auto shadow-md sm:rounded-lg mt-32">
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        {/* Table header */}
         <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
           <tr>
             <th scope="col" className="px-6 py-3">
@@ -156,6 +156,9 @@ function EventEdit() {
               Club Name
             </th>
             <th scope="col" className="px-6 py-3">
+              Mini Description
+            </th>
+            <th scope="col" className="px-6 py-3">
               Description
             </th>
             <th scope="col" className="px-6 py-3">
@@ -169,7 +172,6 @@ function EventEdit() {
             </th>
           </tr>
         </thead>
-        {/* Table body */}
         <tbody>
           {events.map((event) => (
             <tr key={event.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
@@ -186,6 +188,9 @@ function EventEdit() {
                 {event.clubName}
               </td>
               <td className="px-6 py-4">
+                {event.minidescription}
+              </td>
+              <td className="px-6 py-4">
                 {event.description}
               </td>
               <td className="px-6 py-4">
@@ -195,7 +200,6 @@ function EventEdit() {
                 <button onClick={() => handleEdit(event)} className="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</button>
               </td>
               <td className="px-6 py-4">
-                {/* Delete Button */}
                 <button onClick={() => handleDelete(event.id)} className="font-medium text-red-600 dark:text-red-500 hover:underline">Delete</button>
               </td>
             </tr>
@@ -213,12 +217,12 @@ function EventEdit() {
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
           <div className="bg-white p-8 rounded-lg shadow-md">
             <h2 className="text-lg font-semibold mb-4">Edit Event</h2>
-            {/* Form for editing event */}
             <form onSubmit={handleSubmit} className="mt-4">
               <input type="text" className="m-3" id="eventName" value={formData.eventName} onChange={handleChange} placeholder="Event Name" required />
               <input type="text" className="m-3" id="date" value={formData.date} onChange={handleChange} placeholder="Date" required />
               <input type="text" className="m-3" id="time" value={formData.time} onChange={handleChange} placeholder="Time" required />
               <input type="text" className="m-3" id="clubName" value={formData.clubName} onChange={handleChange} placeholder="Club Name" required />
+              <input type="text" className="m-3" id="minidescription" value={formData.minidescription} onChange={handleChange} placeholder="Mini Description" required />
               <input type="text" className="m-3" id="description" value={formData.description} onChange={handleChange} placeholder="Description" required />
               <input type="file" className="m-3" id="image" onChange={handleChange} accept="image/*" />
               <button type="submit" className="bg-green-600 hover:bg-green-700 text-white font-medium py-2 px-4 rounded-lg transition duration-300">Save Changes</button>
